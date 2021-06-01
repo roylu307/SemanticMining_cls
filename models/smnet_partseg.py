90dc11adc5b69155a6d8f1d1ec8ba9c40ac7dde2
roylu307 <50252343+roylu307@users.noreply.github.com>
2021-06-01 15:48:51 +0100

    Update smnet_partseg.py

--- models/smnet_partseg.py
+++ models/smnet_partseg.py
@@ -18,8 +18,8 @@ from scipy.stats import t as student_t
 import statistics as stats
 import math
 
-# DensePoint: 2 PPools + 3 PConvs + 1 global pool; narrowness k = 24; group number g = 2
-class DensePoint(nn.Module):
+
+class SM_net(nn.Module):
 
     def __init__(self, num_classes, input_channels=0, use_xyz=True):
         super().__init__()
